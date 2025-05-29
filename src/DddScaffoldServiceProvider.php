180490c886de617d41de699d3d18fe208e24cde4
ef-tech <okumura@ef-tech.jp>
2025-05-29 21:49:06 +0900

    feat: 📦 Add DddScaffoldServiceProvider and update composer.json for Laravel integration

---
+++ src/DddScaffoldServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace EfTech\DddScaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+class DddScaffoldServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
