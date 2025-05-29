27b830a776c6f967ee3e413de8040e436f16a711
ef-tech <okumura@ef-tech.jp>
2025-05-29 22:06:01 +0900

    feat: ✨ Add InitCommand to generate base DDD structure for projects

---
+++ src/Commands/InitCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace EfTech\DddScaffold\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class InitCommand extends Command
+{
+    protected $signature = 'ddd:init {name=myproject}';
+    protected $description = 'Generate base DDD structure for your project';
+
+    public function handle()
+    {
+        $base = base_path($this->argument('name'));
+
+        $directories = [
+            '/Application/DTOs',
+            '/Application/Exceptions',
+            '/Application/Services',
+            '/Application/UseCases',
+            '/Domain/Exceptions',
+            '/Domain/Models',
+            '/Domain/Repositories',
+            '/Domain/Rules',
+            '/Domain/Services',
+            '/Domain/ValueObjects',
+            '/Infrastructure/Exceptions',
+            '/Infrastructure/Repositories',
+            '/Infrastructure/Services',
+            '/Support/Contracts',
+            '/Support/Enum',
+            '/Support/Exceptions',
+        ];
+
+        foreach ($directories as $dir) {
+            $fullPath = $base . $dir;
+            File::ensureDirectoryExists($fullPath);
+        }
+
+        $this->info('✅ DDD structure scaffolding complete!');
+    }
+}
