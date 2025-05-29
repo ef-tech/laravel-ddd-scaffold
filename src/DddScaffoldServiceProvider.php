8ed7be0edc2d004593a0625fa1f72f33a02b577f
ef-tech <okumura@ef-tech.jp>
2025-05-29 22:06:28 +0900

    feat: 🛠️ Register InitCommand in DddScaffoldServiceProvider for console usage

--- src/DddScaffoldServiceProvider.php
+++ src/DddScaffoldServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace EfTech\DddScaffold;
 
+use EfTech\DddScaffold\Commands\InitCommand;
 use Illuminate\Support\ServiceProvider;
 
 class DddScaffoldServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InitCommand::class,
+            ]);
+        }
     }
 
     public function boot(): void
